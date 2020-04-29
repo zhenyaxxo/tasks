18c495fea32fb92b32696ffdcd02faf5db523293
zhenyaxxo <zhenyaxxo@gmail.com>
2020-04-29 21:58:40 +0500

    task 5 comlipte

--- Assets/Scripts/task_05.cs
+++ Assets/Scripts/task_05.cs
@@ -4,26 +4,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-namespace task_05
+
+public class task_05 : MonoBehaviour
 {
-    public class task_05 : MonoBehaviour
+    //Кубы будем распологать по оси X
+
+    public int N = 5;
+    public int i = 0;
+    private bool Error = false;            //Переменная для того чтобы понять есть ли куб на координате или нет
+    private List<int> _usedCoord = new List<int>();
+    private void Start()
     {
-        //Кубы будем распологать по оси X
-
-        public int N = 5;
-        public int i = 0;
-
-        private void Start()
+        for (; i < N; i++)
         {
-            for (; i < N; i++)
+            int x = Random.Range(0, N * 2);
+            for (int j = 0; j < _usedCoord.Count; j++)
             {
-                Vector3 temp = new Vector3(Random.Range(0, N * 2), 0, 0);
+                if (_usedCoord[j] == x)
+                {
+                    Error = true;
+                    break;
+                }
+                else Error = false;
+            }
+
+            if (Error == false)           //Если такой координаты нет в листе, то тогда создай куб, если есть, то пройди еще раз цикл с тем же значением i
+            {
+                Vector3 temp = new Vector3(x, 0, 0);
                 GameObject Cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 Cube.transform.position = temp;
-                BoxCollider CubeCollider = Cube.GetComponent<BoxCollider>();
-                CubeCollider.isTrigger = true;
-                Cube.AddComponent<task_05_trigger>();
+                _usedCoord.Add(x);
             }
+            else i--;
         }
     }
 }
